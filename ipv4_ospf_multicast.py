330019d32ab1940ed8bbfd553b1fbd6b1e83b630
Ashish Jain <ash.1382@gmail.com>
2017-09-23 21:19:00 +0530

    Fixed bugs + added support for cluster creation & spine links disablement

--- ipv4_ospf_multicast.py
+++ ipv4_ospf_multicast.py
@@ -7,10 +7,10 @@ import sys
 ##################
 # Constants
 ##################
-loopback_ip = "99.99.99."
+g_loopback_ip = "99.99.99."
+g_cluster_vlan = 4040
 ipv6_start = 15360
 ipv6_end = 15480
-cluster_vlan = 4040
 
 ##################
 # ARGUMENT PARSING
@@ -45,7 +45,7 @@ args = vars(parser.parse_args())
 if not args['prefix']:
     args['prefix']="2607:f4a0:3:0:250:56ff:feac:" if args['type'] == "ipv6" else "192.168.100."
 
-spine_list = [i.strip() for i in args['spine'].split(',')]
+g_spine_list = [i.strip() for i in args['spine'].split(',')]
 
 ################
 # UTIL FUNCTIONS
@@ -77,62 +77,95 @@ def run_cmd(cmd):
 
 ################
 
-# Get Router-ID info
-rid_info = {}
-fnodes = run_cmd("fabric-node-show format name parsable-delim ,")
-i = 1
-for sw in spine_list:
-    if sw not in fnodes:
+# Get list of fabric nodes
+g_fab_nodes = []
+g_fab_name = ""
+fab_info = run_cmd("fabric-node-show format name,fab-name parsable-delim ,")
+for finfo in fab_info:
+    if not finfo:
+        print("No fabric output")
+        exit(0)
+    sw_name, fab_name = finfo.split(',')
+    g_fab_nodes.append(sw_name)
+    g_fab_name = fab_name
+
+# Validate spine list
+for sw in g_spine_list:
+    if sw not in g_fab_nodes:
         print("Incorrect spine name %s" % sw)
         exit(0)
-for sw in fnodes:
-    rid_info[sw] = loopback_ip + str(i)
+
+# Get Router-ID info
+g_rid_info = {}
+i = 1
+for sw in g_fab_nodes:
+    g_rid_info[sw] = g_loopback_ip + str(i)
     i += 1
 
-cluster_cmd = "cluster-show format cluster-node-1,cluster-node-2, parsable-delim ,"
-cluster_nodes = []
-for cl in run_cmd(cluster_cmd):
-    if not cl:
-        print("No clusters found")
-        exit(0)
-    cls1,cls2 = cl.split(',')
-    cluster_nodes.append((cls1,cls2))
-
+'''
 # Enable all ports to get better visibility of the topology
-for sw in fnodes:
-    if sw in spine_list:
+for sw in g_fab_nodes:
+    if sw in g_spine_list:
         continue
     run_cmd("switch %s port-config-modify port all enable" %sw)
 time.sleep(5)
+'''
 
-# Get Connected Links (not part of cluster)
-links = []
-cluster_links = []
+# Get all the connected links - sw1,p1,p2,sw2
+g_main_links = []
 lldp_cmd = "lldp-show format switch,local-port,port-id,sys-name parsable-delim ,"
 for conn in run_cmd(lldp_cmd):
     if not conn:
         print("No LLDP output")
         exit(0)
-    sw1,p1,p2,sw2 = conn.split(',')
+    g_main_links.append(conn.split(','))
+
+g_cluster_nodes = set()
+for conn in g_main_links:
+    sw1,p1,p2,sw2 = conn
+    # Disable spine ports
+    if sw1 in g_spine_list and sw2 in g_spine_list:
+        print("Disabling spine link on %s. Port: %s" % (sw1,p1))
+        run_cmd("switch %s port-config-modify port %s disable" % (sw1,p1))
+    # Get cluster node list
+    if sw1 not in g_spine_list and sw2 not in g_spine_list:
+        if (sw1,sw2) not in g_cluster_nodes and (sw2,sw1) not in g_cluster_nodes:
+            g_cluster_nodes.add((sw1,sw2))
+
+# Create Clusters
+if len(g_cluster_nodes) == 0:
+    print("No cluster nodes found")
+    exit(0)
+i = 1
+for c_node in g_cluster_nodes:
+    sw1,sw2 = c_node
+    print("Creating cluster: cluster-leaf%d between %s & %s" % (i,sw1,sw2))
+    run_cmd("cluster-create name cluster-leaf%d cluster-node-1 %s cluster-node-2 %s" % (i,sw1,sw2))
+    i += 1
+
+# Get Connected Links (not part of cluster)
+g_l3_links = []
+for conn in g_main_links:
+    sw1,p1,p2,sw2 = conn
     # Skip Clustered links
-    if (sw1,sw2) in cluster_nodes or (sw2,sw1) in cluster_nodes:
+    if (sw1,sw2) in g_cluster_nodes or (sw2,sw1) in g_cluster_nodes:
         continue
     # Skip non fabric nodes
-    if sw1 not in fnodes or sw2 not in fnodes:
+    if sw1 not in g_fab_nodes or sw2 not in g_fab_nodes:
         continue
-    if (sw2,p2,p1,sw1) not in links:
-        links.append((sw1,p1,p2,sw2))
+    # Skip spine links
+    if sw1 in g_spine_list and sw2 in g_spine_list:
+        continue
+    if (sw2,p2,p1,sw1) not in g_l3_links:
+        g_l3_links.append((sw1,p1,p2,sw2))
 
 # Create vRouters
-sw_cmd = "fabric-node-show format name,fab-name parsable-delim ,"
-sw_details = run_cmd(sw_cmd)
-for swinfo in sw_details:
-    swname, fabname = swinfo.split(',')
+for swname in g_fab_nodes:
     print("Creating vRouter %s-vrouter on %s..." %(swname, swname), end='')
     sys.stdout.flush()
     run_cmd("switch %s vrouter-create name %s-vrouter vnet %s-global router-type "
             "hardware router-id %s proto-multi pim-ssm ospf-redistribute connected" % (
-            swname,swname,fabname,rid_info[swname]))
+            swname,swname,g_fab_name,g_rid_info[swname]))
     print("Done")
     sys.stdout.flush()
     time.sleep(20)
@@ -146,7 +179,8 @@ else:
     ip_generator = give_ipv6(ipv6_start,ipv6_end)
     netmask = args['subnet']
     mproto = "ipv6-unicast"
-for link in links:
+print("")
+for link in g_l3_links:
     sw1,p1,p2,sw2 = link
     ip1,ip2 = ip_generator.next().split(',')
     #####vRouter-Interface#####
@@ -182,13 +216,13 @@ for link in links:
     print("Done")
     sys.stdout.flush()
     time.sleep(10)
-
-for sws in cluster_nodes:
+print("")
+for sws in g_cluster_nodes:
     sw1,sw2 = sws
     ####Creation cluster VLANs####
     print("Creating VLAN 4040 cluster scope on switches: %s & %s..." %sws, end='')
     sys.stdout.flush()
-    run_cmd("switch %s vlan-create id %s scope cluster" % (sw1,cluster_vlan))
+    run_cmd("switch %s vlan-create id %s scope cluster" % (sw1,g_cluster_vlan))
     print("Done")
     sys.stdout.flush()
     time.sleep(1)
@@ -196,9 +230,9 @@ for sws in cluster_nodes:
     ip1,ip2 = ip_generator.next().split(',')
     ########################################
     #####vRouter-Interface#####
-    print("Adding vRouter interface to vrouter=%s-vrouter vlan=%s ip=%s/%s..." %(sw1,cluster_vlan,ip1,netmask), end='')
+    print("Adding vRouter interface to vrouter=%s-vrouter vlan=%s ip=%s/%s..." %(sw1,g_cluster_vlan,ip1,netmask), end='')
     sys.stdout.flush()
-    run_cmd("vrouter-interface-add vrouter-name %s-vrouter vlan %s ip %s/%s mtu 9216 pim-cluster" %(sw1,cluster_vlan,ip1,netmask))
+    run_cmd("vrouter-interface-add vrouter-name %s-vrouter vlan %s ip %s/%s mtu 9216 pim-cluster" %(sw1,g_cluster_vlan,ip1,netmask))
     print("Done")
     sys.stdout.flush()
     time.sleep(2)
@@ -211,9 +245,9 @@ for sws in cluster_nodes:
     time.sleep(5)
     ########################################
     #####vRouter-Interface#####
-    print("Adding vRouter interface to vrouter=%s-vrouter vlan=%s ip=%s/%s..." %(sw2,cluster_vlan,ip2,netmask), end='')
+    print("Adding vRouter interface to vrouter=%s-vrouter vlan=%s ip=%s/%s..." %(sw2,g_cluster_vlan,ip2,netmask), end='')
     sys.stdout.flush()
-    run_cmd("vrouter-interface-add vrouter-name %s-vrouter vlan %s ip %s/%s mtu 9216 pim-cluster" %(sw2,cluster_vlan,ip2,netmask))
+    run_cmd("vrouter-interface-add vrouter-name %s-vrouter vlan %s ip %s/%s mtu 9216 pim-cluster" %(sw2,g_cluster_vlan,ip2,netmask))
     print("Done")
     sys.stdout.flush()
     time.sleep(2)
