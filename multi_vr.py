cd13ff20423221885d296bc84741739a52f04cb3
Ashish Jain <ashish.jain@mobiledgex.com>
2019-03-21 14:32:22 +0530

    Ability to run it remotely

--- multi_vr.py
+++ multi_vr.py
@@ -11,13 +11,13 @@ import sys
 
 parser = argparse.ArgumentParser(description='Multi-vRouter Setup Script')
 parser.add_argument(
-    '--swn',
-    help='switch to multiple vrouters',
+    '--swn-ip',
+    help='IP for switch to multiple vrouters',
     required=True
 )
 parser.add_argument(
-    '--sw1',
-    help='switch to host 1 vrouter',
+    '--sw1-ip',
+    help='IP for switch to host 1 vrouter',
     required=True
 )
 parser.add_argument(
@@ -29,8 +29,8 @@ parser.add_argument(
 args = vars(parser.parse_args())
 
 show_only = args["show_only"]
-g_swn = args["swn"]
-g_sw1 = args["sw1"]
+g_swn = args["swn_ip"]
+g_sw1 = args["sw1_ip"]
 g_vr_count = 32
 g_ip_prefix = "192.168.1."
 g_ip_netmask = "/30"
@@ -39,6 +39,15 @@ g_ip_netmask = "/30"
 # UTIL FUNCTIONS
 #===============================================================================
 
+def ping_test(ip_addr):
+    cmd = ("ping -c 1 %s" % (ip_addr))
+
+    message = run_local_cmd(cmd)
+    message = '\n'.join(message)
+    if ('0.0% packet loss' in message):
+        return True
+    return False
+
 def ignore_output_str(output):
     if "already" in output:
         return True
@@ -46,14 +55,24 @@ def ignore_output_str(output):
         return True
     return False
 
-def run_cmd(cmd, ignore_err=False):
+def run_local_cmd(cmd):
+    try:
+        proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+        output = proc.communicate()[0]
+        return output.strip().split('\n')
+    except:
+        print_err("Failed running cmd %s" % cmd)
+
+def run_cmd(cmd, switch, ignore_err=False):
     ignore_err_list = [errno.EEXIST]
-    m_cmd = "cli --quiet --no-login-prompt --user network-admin:test123 " + cmd
+    m_cmd = ("sshpass -p test123 ssh -q -oStrictHostKeyChecking=no "
+       "-oConnectTimeout=10 network-admin@%s -- --quiet %s "
+       "2>&1" % (switch, cmd))
     if show_only and "-show" not in cmd:
         print("### " + cmd)
         return
     try:
-        proc = subprocess.Popen(m_cmd + " 2>&1", shell=True, stdout=subprocess.PIPE)
+        proc = subprocess.Popen(m_cmd, shell=True, stdout=subprocess.PIPE)
         output,err = proc.communicate()
         if not ignore_err and \
            proc.returncode and \
@@ -96,35 +115,17 @@ def _print(msg, end="nl", must_show=False):
     sys.stdout.flush()
 
 #===============================================================================
-# Validation
-#===============================================================================
-
-# Get list of fabric nodes
-g_fab_nodes = []
-g_fab_name = ""
-fab_info = run_cmd("fabric-node-show format name,fab-name parsable-delim ,")
-for finfo in fab_info:
-    if not finfo:
-        print("No fabric output")
-        exit(0)
-    sw_name, fab_name = finfo.split(',')
-    g_fab_nodes.append(sw_name)
-    g_fab_name = fab_name
-
-# Validate switches
-if g_swn not in g_fab_nodes:
-    print("Switch %s is not in fabric %s" % (g_swn, g_fab_name))
-    exit(0)
-if g_sw1 not in g_fab_nodes:
-    print("Switch %s is not in fabric %s" % (g_sw1, g_fab_name))
-    exit(0)
-
-#===============================================================================
 # Multi-vRouter & Single-vRouter Configuration
 #===============================================================================
+if not ping_test(g_swn):
+    print("Switch %s is not reachable" % g_swn)
+    exit(0)
+if not ping_test(g_sw1):
+    print("Switch %s is not reachable" % g_sw1)
+    exit(0)
 
 existing_vnets = []
-vnet_info = run_cmd("vnet-show format name parsable-delim ,")
+vnet_info = run_cmd("vnet-show format name parsable-delim ,", g_swn)
 for vinfo in vnet_info:
     if not vinfo:
         break
@@ -132,7 +133,7 @@ for vinfo in vnet_info:
     existing_vnets.append(vn_name)
 
 existing_vrouters = []
-vrouter_info = run_cmd("vrouter-show format name parsable-delim ,")
+vrouter_info = run_cmd("vrouter-show format name parsable-delim ,", g_swn)
 for vinfo in vrouter_info:
     if not vinfo:
         break
@@ -151,7 +152,7 @@ for vr_index in range(1, g_vr_count+1):
     if vnname not in existing_vnets:
         _print("Creating vNET %s on %s..." % (vnname, g_swn), end='')
         run_cmd("switch %s vnet-create name %s scope fabric" % (
-                    g_swn, vnname))
+                    g_swn, vnname), g_swn)
         sleep(1)
         _print("Done")
 
@@ -161,13 +162,13 @@ for vr_index in range(1, g_vr_count+1):
         continue
     _print("Creating vRouter %s on %s..." % (vrname, g_swn), end='')
     run_cmd("switch %s vrouter-create name %s vnet %s "
-            "router-type hardware" % (g_swn, vrname, vnname))
+            "router-type hardware" % (g_swn, vrname, vnname), g_swn)
     sleep(1)
     _print("Done")
 
     vlan_id = 100 + vr_index
     _print("Creating vlan, local scoped: %d on %s" % (vlan_id, g_swn))
-    run_cmd("switch %s vlan-create id %d scope local" % (g_swn, vlan_id))
+    run_cmd("switch %s vlan-create id %d scope local" % (g_swn, vlan_id), g_swn)
     sleep(1)
     _print("Done")
 
@@ -175,7 +176,7 @@ for vr_index in range(1, g_vr_count+1):
     _print("Creating interface on vrouter %s with ip %s & vlan %s on "
             "switch %s" % (vrname, vr_ip, vlan_id, g_swn))
     run_cmd("vrouter-interface-add vrouter-name %s ip %s vlan %s" % (
-        vrname, vr_ip, vlan_id))
+        vrname, vr_ip, vlan_id), g_swn)
     sleep(1)
     _print("Done")
     g_vlan_ipindex_list.append((vlan_id, ipindex+1))
@@ -184,12 +185,27 @@ for vr_index in range(1, g_vr_count+1):
     _print("Adding bgp network on %s for %s on switch %s..." % (
         vrname, vr_ip, g_swn))
     run_cmd("switch %s vrouter-bgp-network-add vrouter-name %s "
-            "network %s" % (g_swn, vrname, vr_ip))
+            "network %s" % (g_swn, vrname, vr_ip), g_swn)
     sleep(2)
     _print("Done")
     _print("")
 
 #===============================================================================
+existing_vnets = []
+vnet_info = run_cmd("vnet-show format name parsable-delim ,", g_sw1)
+for vinfo in vnet_info:
+    if not vinfo:
+        break
+    vn_name = vinfo
+    existing_vnets.append(vn_name)
+
+existing_vrouters = []
+vrouter_info = run_cmd("vrouter-show format name parsable-delim ,", g_sw1)
+for vinfo in vrouter_info:
+    if not vinfo:
+        break
+    vr_name = vinfo
+    existing_vrouters.append(vr_name)
 _print("")
 _print("### Configuring single vRouter", must_show=True)
 _print("### ==========================", must_show=True)
@@ -198,7 +214,7 @@ vnname = g_sw1 + "-vn"
 if vnname not in existing_vnets:
     _print("Creating vNET %s on %s..." % (vnname, g_sw1), end='')
     run_cmd("switch %s vnet-create name %s scope fabric" % (
-                g_sw1, vnname))
+                g_sw1, vnname), g_sw1)
     sleep(1)
     _print("Done")
 
@@ -206,14 +222,14 @@ vrname = g_sw1 + "-vr"
 if vrname not in existing_vrouters:
     _print("Creating vRouter %s on %s..." % (vrname, g_sw1), end='')
     run_cmd("switch %s vrouter-create name %s vnet %s "
-            "router-type hardware" % (g_sw1, vrname, vnname))
+            "router-type hardware" % (g_sw1, vrname, vnname), g_sw1)
     sleep(1)
     _print("Done")
 
 for entry in g_vlan_ipindex_list:
     vlan_id, ipindex = entry
     _print("Creating vlan, local scoped: %d on %s" % (vlan_id, g_sw1))
-    run_cmd("switch %s vlan-create id %d scope local" % (g_sw1, vlan_id))
+    run_cmd("switch %s vlan-create id %d scope local" % (g_sw1, vlan_id), g_sw1)
     sleep(1)
     _print("Done")
 
@@ -221,14 +237,14 @@ for entry in g_vlan_ipindex_list:
     _print("Creating interface on vrouter %s with ip %s & vlan %s on "
             "switch %s" % (vrname, vr_ip, vlan_id, g_sw1))
     run_cmd("vrouter-interface-add vrouter-name %s ip %s vlan %s" % (
-        vrname, vr_ip, vlan_id))
+        vrname, vr_ip, vlan_id), g_sw1)
     sleep(1)
     _print("Done")
 
     _print("Adding bgp network on %s for %s on switch %s..." % (
         vrname, vr_ip, g_sw1))
     run_cmd("switch %s vrouter-bgp-network-add vrouter-name %s "
-            "network %s" % (g_sw1, vrname, vr_ip))
+            "network %s" % (g_sw1, vrname, vr_ip), g_sw1)
     sleep(2)
     _print("Done")
     _print("")
